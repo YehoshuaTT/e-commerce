46d1e0fd109b292f908fc4bca4182eeb4b4005ba
Yehoshua Tarshish <a0526741852@gmail.com>
2024-03-18 00:24:21 +0200

    added social nav butttons

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import { inclusions, noHeaderFooterUrls } from 'app/constansts'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from 'app/constansts'
 import classes from './index.module.scss'
 import React from 'react'
 import { usePathname } from 'next/navigation'
 import { Gutter } from 'app/_components/Gutter'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Footer } from 'payload/payload-types'
+import { Media } from 'payload/payload-types'
+import { Footer } from '../../../../payload/payload-types'
+import { Button } from 'payload/components'
 
 const FooterComponent = ({ footer }: { footer: Footer }) => {
   const pathname = usePathname()
+  const navItems = footer?.navItems || []
 
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
@@ -37,7 +40,33 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
             <Link href="/">
               <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
             </Link>
-            <p>{footer.copyright}</p>
+
+            <p>{footer?.copyright}</p>
+
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = item?.link?.icon as Media
+
+                return (
+                  <a
+                    key={item.link.label}
+                    // el="link"
+                    href={item.link.url}
+                    target="_blank"
+                    // newTab={true}
+                    className={classes.socialLinkItem}
+                  >
+                    <Image
+                      src={icon?.url}
+                      alt={item.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    ></Image>
+                  </a>
+                )
+              })}
+            </div>
           </div>
         </Gutter>
       </div>
